73572984abffd3e923a8b0f6a8a4da07595bb719
thefipster <felix.reisch@live.de>
2025-10-21 22:15:27 +0200

    Refactor AppHost to use Harvester Web project
    Removed references and code for ActivityAggregator and Strive systems from AppHost. Updated AppHost to health check the Harvester Web project.

--- src/Fip.Strive.AppHost/AppHost.cs
+++ src/Fip.Strive.AppHost/AppHost.cs
@@ -1,7 +1,7 @@
-using Fip.Strive.AppHost.Systems;
-
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddStriveProject();
+builder
+    .AddProject<Projects.Fip_Strive_Harvester_Web>("strive-harvester-webapp")
+    .WithHttpHealthCheck("/health");
 
 builder.Build().Run();
