ce450488b633b9d816c5b26ca49b0bd38838b0c1
thefipster <felix.reisch@live.de>
2025-10-25 09:32:02 +0200

    Added build script and test coverage report
    Added a PowerShell build script (make.ps1) for common tasks. Introduced Directory.Build.props chaining for subprojects. Enhanced health checks in AppHost.

---
+++ make.ps1
@@ -0,0 +1,65 @@
+param (
+    [Parameter(Mandatory = $true)]
+    [ValidateSet("build", "run", "test", "clean")]
+    [string]$Command
+)
+
+# Stop execution on all errors
+$ErrorActionPreference = 'Stop'
+
+# Don't display progress UI (some commands show progress bars that clutter logs)
+$ProgressPreference = 'SilentlyContinue'
+
+function Build-Strive {
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string]$Args
+    )
+
+    Write-Host "Building"
+    dotnet build strive.sln --configuration Release
+}
+
+function Run-Strive {
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string]$Args
+    )
+    Write-Host "Starting"
+    dotnet run --project src/Fip.Strive.AppHost --configuration Release
+}
+
+function Clean-Strive {
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string]$Args
+    )
+    Write-Host "Cleaning"
+    dotnet clean
+}
+
+function Test-Strive {
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string]$Args
+    )
+    Write-Host "Testing"
+    dotnet test --collect:"XPlat Code Coverage"
+    reportgenerator -reports:**/coverage*.xml -targetdir:coverage-report -reporttypes:Html
+    Start-Process (Join-Path $PSScriptRoot 'coverage-report\index.html')
+}
+
+switch ($Command) {
+    "build" {
+        Build-Strive -Args "."
+    }
+    "run" {
+        Run-Strive -Args "."
+    }
+    "test" {
+        Test-Strive -Args "."
+    }
+    "clean" {
+        Clean-Strive -Args "."
+    }
+}
