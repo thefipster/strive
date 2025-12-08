1a607ff30b8dd34ac51c2e68cee2df2939e1ecb4
thefipster <felix.reisch@live.de>
2025-12-08 23:22:15 +0100

    Add initial migration for PipelineContext

---
+++ src/harvest/Fip.Strive.Harvester.Application/Infrastructure/Pipeline/Migrations/20251208171442_InitialCreate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fip.Strive.Harvester.Application.Infrastructure.Pipeline.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "harvester-pipeline");
+
+            migrationBuilder.CreateTable(
+                name: "Quarantined",
+                schema: "harvester-pipeline",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "gen_random_uuid()"),
+                    CreatedUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Stacktrace = table.Column<string>(type: "text", nullable: true),
+                    ErrorMessage = table.Column<string>(type: "text", nullable: true),
+                    ErrorType = table.Column<string>(type: "text", nullable: true),
+                    SignalType = table.Column<int>(type: "integer", nullable: false),
+                    Payload = table.Column<string>(type: "jsonb", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quarantined", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Quarantined",
+                schema: "harvester-pipeline");
+        }
+    }
+}
